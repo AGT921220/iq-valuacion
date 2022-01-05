60ba2238271de4b63d586794ee5a8cebfb68e3ae
Alfredo Gutierrez <alfredo.gutierrez_jr.0792@hotmail.com>
2022-01-04 18:32:28 -0600

    add:users Seeder

---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,42 @@
+<?php
+use App\User;
+use App\Domicilio;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $adminUser = new User();
+        $adminUser->name='Admin';
+        $adminUser->email='admin@iq-valuacion.com';
+        $adminUser->telefono='6141234567';
+        $adminUser->password =Hash::make('agt123');
+        $adminUser->save();
+        // for ($i=1; $i <= 100 ; $i++) { 
+        //     $user = new User();
+        //     $user->name ='Name'.$i;
+        //     $user->email='email'.$i.'@test.com';
+        //     $user->password =Hash::make('Password_capi_'.$i);
+        //     $user->fecha_nacimiento=Carbon::today()->subDays(rand(0, 10000));
+        //     $user->save();
+        //     $domicilio = new Domicilio();
+        //     $domicilio->user_id =$i;
+        //     $domicilio->domicilio='Calle '.$i;
+        //     $domicilio->numero_exterior =$i;
+        //     $domicilio->cp ='310'.$i;
+        //     $domicilio->ciudad='Chihuahua';
+        //     $domicilio->save();
+        // }
+
+
+    }
+}
