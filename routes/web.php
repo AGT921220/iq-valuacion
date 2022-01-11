2e2278f9190286ef42522d279770a90216893b48
Alfredo Gutierrez <alfredo.gutierrez_jr.0792@hotmail.com>
2022-01-11 00:31:21 -0600

    layout blade

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
-use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
+
 Route::get('/home', 'HomeController@index')->name('home');
 
 //PERFIL DE USUARIO
