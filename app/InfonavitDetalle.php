2902ca7427dd9bc21645553f9018c39db0a107ed
Luis S. Salgado <luiss.salgado@MacBook-Pro-de-Luis.local>
2022-01-20 19:45:29 -0700

    migracion detalle infonavit,modelo

---
+++ app/InfonavitDetalle.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class InfonavitDetalle extends Model
+{
+    protected $fillable = [
+        'solicitante_nombre',
+        'solicitante_ap',
+        'solicitante_am',
+        'solicitante_rfc',
+        'solicitante_curp',
+        'solicitante_nss',
+        'solicitante_telefono',
+        'solicitante_email',
+        'solicitante_domicilio_calle',
+        'solicitante_domicilio_numero',
+        'solicitante_domicilio_interior',
+        'solicitante_domicilio_colonia',
+        'solicitante_domicilio_municipio',
+        'solicitante_domicilio_estado',
+        'solicitante_domicilio_cp',
+        'vendedor_nombre',
+        'vendedor_ap',
+        'vendedor_am',
+        'vendedor_rfc',
+        'vendedor_curp',
+        'vendedor_nss',
+        'vendedor_telefono',
+        'vendedor_email',
+        'vendedor_domicilio_calle',
+        'vendedor_domicilio_numero',
+        'vendedor_domicilio_interior',
+        'vendedor_domicilio_colonia',
+        'vendedor_domicilio_municipio',
+        'vendedor_domicilio_estado',
+        'vendedor_domicilio_cp',
+        'vivienda_cuv',
+        'vivienda_cuenta_predial',
+        'vivienda_calle',
+        'vivienda_numero',
+        'vivienda_interior',
+        'vivienda_colonia',
+        'calles_referencia',
+        'vivienda_municipio',
+        'vivienda_estado',
+        'vivienda_cp',
+        'vivienda_edificio',
+        'vivienda_nivel',
+       'vivienda_entrada',
+        'escritura_nombre_notario',
+       'escritura_notario_numero',
+        'escritura_notario_distrito',
+        'escritura_escritura',
+        'escritura_volumen',
+        'escritura_folio_real',
+        'escritura_insc',
+        'escritura_folio',
+        'escritura_libro',
+        'confirmacion_visita_nombre',
+        'confirmacion_visita_celular',
+        'confirmacion_visita_correo'
+    ];
+}
---
+++ database/migrations/2022_01_21_022153_create_detaills_infonativ_table.php
@@ -0,0 +1,87 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetaillsInfonativTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detaills_infonativ', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('solicitante_nombre');
+            $table->string('solicitante_ap');
+            $table->string('solicitante_am');
+            $table->string('solicitante_rfc');
+            $table->string('solicitante_curp');
+            $table->string('solicitante_nss');
+            $table->string('solicitante_telefono');
+            $table->string('solicitante_email');
+            $table->string('solicitante_domicilio_calle');
+            $table->string('solicitante_domicilio_numero');
+            $table->string('solicitante_domicilio_interior');
+            $table->string('solicitante_domicilio_colonia');
+            $table->string('solicitante_domicilio_municipio');
+            $table->string('solicitante_domicilio_estado');
+            $table->string('solicitante_domicilio_cp');
+            $table->string('vendedor_nombre');
+            $table->string('vendedor_ap');
+            $table->string('vendedor_am');
+            $table->string('vendedor_rfc');
+            $table->string('vendedor_curp');
+            $table->string('vendedor_nss');
+            $table->string('vendedor_telefono');
+            $table->string('vendedor_email');
+            $table->string('vendedor_domicilio_calle');
+            $table->string('vendedor_domicilio_numero');
+            $table->string('vendedor_domicilio_interior');
+            $table->string('vendedor_domicilio_colonia');
+            $table->string('vendedor_domicilio_municipio');
+            $table->string('vendedor_domicilio_estado');
+            $table->string('vendedor_domicilio_cp');
+            $table->string('vivienda_cuv');
+            $table->string('vivienda_cuenta_predial');
+            $table->string('vivienda_calle');
+            $table->string('vivienda_numero');
+            $table->string('vivienda_interior');
+            $table->string('vivienda_colonia');
+            $table->string('calles_referencia')->nullable();
+            $table->string('vivienda_municipio');
+            $table->string('vivienda_estado');
+            $table->string('vivienda_cp');
+            $table->string('vivienda_edificio')->nullable();
+            $table->string('vivienda_nivel')->nullable();
+            $table->string('vivienda_entrada')->nullable();
+            $table->string('escritura_nombre_notario');
+            $table->string('escritura_notario_numero');
+            $table->string('escritura_notario_distrito');
+            $table->string('escritura_escritura');
+            $table->string('escritura_volumen');
+            $table->string('escritura_folio_real');
+            $table->string('escritura_insc');
+            $table->string('escritura_folio');
+            $table->string('escritura_libro');
+            $table->string('confirmacion_visita_nombre');
+            $table->string('confirmacion_visita_celular');
+            $table->string('confirmacion_visita_correo');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detaills_infonativ');
+    }
+}
